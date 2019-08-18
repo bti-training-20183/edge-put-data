da3eb29746a9e6afe9a06fe3a5ef8d401b001991
Quang Dang <42106752+Homuru@users.noreply.github.com>
2019-08-18 21:37:12 +0700

    Added cron

--- crawl.py
+++ crawl.py
@@ -4,16 +4,22 @@ import json
 import sys
 sys.path.append(os.getcwd())
 import config
-
+import time
+import csv
 
 
 params = {
     "function" : "TIME_SERIES_DAILY",
     "symbol" :  "MSFT",
-    "outputsize" : "compact",
-    "datatype" : "CSV",
-    "apikey" : config.API_KEY
+    "apikey" : config.API_KEY,
+    "datatype" : "csv"
 }
 
-r = requests.get(config.API_ENDPOINT, params=params)
-print(r.url)
+response = requests.get(config.API_ENDPOINT, params=params)
+print(response.url)
+filename = 'test'+'.csv'
+with open(filename, 'w') as f:
+    writer = csv.writer(f)
+    for line in response.iter_lines():
+        writer.writerow(line.decode('utf-8').split(','))
+
