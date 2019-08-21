e7251b2ad05120b8d5e35689d93f28854a2d5f12
Quang Dang <42106752+Homuru@users.noreply.github.com>
2019-08-21 16:37:31 +0700

    Data preprocess

--- crawl.py
+++ crawl.py
@@ -1,45 +1,53 @@
+from utils.database_handler import Database_Handler
+from utils.datastore_handler import Minio_Handler
+import schedule
+import csv
+import time
+import config
 import requests
 import os
 import json
 import sys
 sys.path.append(os.getcwd())
-import config
-import time
-import csv
-import schedule
-from utils.datastore_handler import Minio_Handler
-from utils.database_handler import Database_Handler
 
 params = {
-    "function" : "TIME_SERIES_DAILY",
-    "symbol" :  "MSFT",
-    "apikey" : config.API_KEY,
-    "datatype" : "csv"
+    "function": "TIME_SERIES_DAILY",
+    "symbol":  "MSFT",
+    "apikey": config.API_KEY,
+    "datatype": "csv"
 }
+
 
 def crawl_data(params, filename):
     response = requests.get(config.API_ENDPOINT, params=params)
     print(response.url)
     with open(filename, 'w') as f:
         writer = csv.writer(f)
-        for line in response.iter_lines():
-            writer.writerow(line.decode('utf-8').split(','))
+        for i, line in enumerate(response.iter_lines()):
+            if i == 6:
+                break
+            if i >= 1:
+                writer.writerow(line.decode('utf-8').split(',')[1:])
+
 
 def save(filename):
     to_path = 'data/' + filename
-    Minio_Handler.upload(filename,to_path)
+    Minio_Handler.upload(filename, to_path)
     logs = {
-        "filename" : filename,
-        "file_uri" : to_path
+        "name": filename,
+        "file_uri": to_path
     }
     os.remove(filename)
     Database_Handler.insert(config.MONGO_COLLECTION, logs)
-    
+
+
 def do_job(params, filename):
-    crawl_data(params,filename)
+    crawl_data(params, filename)
     save(filename)
 
-schedule.every(15).seconds.do(do_job, params = params, filename = time.ctime()+'.csv')
+
+schedule.every(15).seconds.do(
+    do_job, params=params, filename=time.strftime("%Y%m%d")+'.csv')
 
 while True:
     schedule.run_pending()
